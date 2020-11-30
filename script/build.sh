7b123eb738196bc4ec43e90a3383ecda6430431a
Devin Don <I.INF@Outlook.com>
2020-11-30 17:41:27 +0800

    chore(build): fix base href

--- script/build.sh
+++ script/build.sh
@@ -10,7 +10,7 @@ react-app-rewired build
 if [ "$TARGET" = "DEMO" ];
 then
   cd build
-  sed -i "s/\/$npm_package_name\//BASE_HREF/g" index.html
+  sed -i "s/$npm_package_name\//BASE_HREF/" index.html
   sed -i "s/$npm_package_name\///g" index.html
   sed -i "s/BASE_HREF/\/$npm_package_name\//g" index.html
 fi
