ddd4b6fb1390751be97a8f1a8acbf6189a5064a6
dev <dev@don.red>
2020-12-09 14:09:44 +0800

    chore(script): update push script

--- script/push.sh
+++ script/push.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+if [ ! -n "$registry" ];
+then
+  registry='docker.io'
+fi
 cd build
 tee > ./Dockerfile <<-'EOF'
 FROM iinfinity/nginx
 COPY ["./", "/app/"]
 EOF
-docker build -t registry.don.red/$npm_package_name .
-docker push registry.don.red/$npm_package_name
+docker build -t $registry/$npm_package_name .
+docker push $registry/$npm_package_name
