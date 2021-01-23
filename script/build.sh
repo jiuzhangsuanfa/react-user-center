5b26a27645702a5ce6b7a1070b2cef0af7891bf3
Devin Don <i.inf@outlook.com>
2021-01-23 16:58:36 +0800

    chore(script): update build script

--- script/build.sh
+++ script/build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-# react-scripts build
-if [ "$TARGET" = "DEMO" ];
-then
-  export PUBLIC_URL=$npm_package_name
-fi
-
 react-app-rewired build
 
 if [ "$TARGET" = "DEMO" ];
 then
   cd build
-  sed -i "s/$npm_package_name\//BASE_HREF/" index.html
-  sed -i "s/$npm_package_name\///g" index.html
-  sed -i "s/BASE_HREF/\/$npm_package_name\//g" index.html
+  sed -i "s/$PUBLIC_URL\//BASE_HREF/" index.html
+  sed -i "s/$PUBLIC_URL\///g" index.html
+  sed -i "s/BASE_HREF/\/$PUBLIC_URL\//g" index.html
 fi
